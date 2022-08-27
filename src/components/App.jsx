453242da66e2a12c2c347805d3917139abeafd6b
David Yoon <davidyoondev@gmail.com>
2022-08-27 14:32:29 -0700

    search component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "../App.css"
 import "../browser_style_reset.css"
+import Search from './search/Search'
 export default function App() {
+    const handleOnSearchChange = (searchData) =>{
+        console.log(searchData);
+    }
   return (
-    <div className='app'>App
+    <div className='app'>
+        <Search onSearchChange={handleOnSearchChange}/>
     </div>
   )
 }
---
+++ src/components/util/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e8703186f2msh08ecc6503da9636p168bfcjsn2228afaf7577',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
